f41cb093e83dc004baa77d4abc3a8833dfe7a054
Roshan Thakur <softdev.roshanthakur@gmail.com>
2025-01-12 02:04:51 +0000

    Create helloWorld.java

---
+++ helloWorld.java
@@ -0,0 +1,10 @@
+// This is the HelloWorld class, which contains the main method
+public class helloWorld {
+
+    // Main method - entry point of the program
+    public static void main(String[] args) {
+        
+        // This line prints "Hello World" to the console
+        System.out.println("Hello World");
+    }
+}
